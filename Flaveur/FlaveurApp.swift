03cdb9da9248c05f9a7e526bc26a895840f892a0
Harsh Khare <harshamt834@gmail.com>
2026-03-08 00:16:33 +0530

    Initial Commit

---
+++ Flaveur/FlaveurApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaveurApp.swift
+//  Flaveur
+//
+//  Created by mac on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FlaveurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
